d2579383cbec1bcc12741694a6e5d28b3a9c540a
jonghoon lee <jonghoon7431@gmail.com>
2024-05-20 19:57:06 +0900

    feat: ex1 과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,24 @@ export default function App() {
     { name: "Diana", age: 19, grade: "D" },
   ];
 
-  // TODO: filter를 사용하여 18세 이상의 학생들만 선택하세요.
-  const filteredStudents = students.filter(/* 여기에 코드 작성 */);
+  const filteredStudents = students.filter((students) => students.age >= 18);
 
   return (
     <div>
       <h1>학생 목록</h1>
       <ul>
-        {/* TODO: map을 사용해서 filteredStudents를 여기에 렌더링하세요. */}
-        {/* TODO: 학생이름을 클릭하면 나이와 점수가 alert 돼야 해요.*/}
+        {filteredStudents.map((students) => (
+          <li key={crypto.randomUUID()}>
+            <p
+              onClick={() => {
+                alert(`age: ${students.age}, grade: ${students.grade}`);
+              }}
+              className="name"
+            >
+              name: {students.name}
+            </p>
+          </li>
+        ))}
       </ul>
     </div>
   );
