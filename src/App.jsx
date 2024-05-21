3abb60edb6c503cac417b2b3ebe20ee5dfedf31d
jonghoon lee <jonghoon7431@gmail.com>
2024-05-21 11:08:37 +0900

    feat: ex4 완료

--- src/App.jsx
+++ src/App.jsx
@@ -15,15 +15,8 @@ function App() {
   return (
     <div>
       <h1>학생 목록</h1>
-      {/* TODO: FilterButtons 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
-      <FilterButtons
-      /* 필요한 props를 여기에 전달하세요. */
-      />
-
-      {/* TODO: StudentList 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
-      <StudentList
-      /* 필요한 props를 여기에 전달하세요. */
-      />
+      <FilterButtons setFilteredStudents={setFilteredStudents} initialStudents={initialStudents} />
+      <StudentList filteredStudents={filteredStudents} />
     </div>
   );
 }
--- src/components/StudentList.jsx
+++ src/components/StudentList.jsx
@@ -1,9 +1,14 @@
 // TODO: StudentList 컴포넌트를 작성하세요. props로 학생 목록을 받아와서 표시해야 합니다.
-function StudentList(/* 필요한 props를 여기에 전달하세요 */ {}) {
+function StudentList({ filteredStudents }) {
   return (
     <ul>
-      {/* 여기에 학생 목록을 표시하는 로직을 작성하세요. */}
-      {/* ex: 홍길동 (20세) - A등급 */}
+      {filteredStudents.map((students) => (
+        <li key={crypto.randomUUID()}>
+          <p>
+            name: {students.name}, age: {students.age}, grade: {students.grade}
+          </p>
+        </li>
+      ))}
     </ul>
   );
 }
